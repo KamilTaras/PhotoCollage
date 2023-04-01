718c4604a8a091ce6df6cec0c9ce178b790b9fec
KamilTaras <kamiltaras0@gmail.com>
2023-04-01 04:21:33 +0200

    actionPerformed() is now divided into smaller functions

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -17,11 +17,12 @@ public class GUI extends JFrame implements ActionListener {
     private static final int WINDOW_WIDTH = 400;
     private static final int WINDOW_HEIGHT = 300;
 
-    private int counter = 0;
-    private JButton openButton, saveButton, makingButton;
+    private JButton openButton, saveButton, makingButton, colorButton;
     private JLabel imageLabel, counterLabel;
 
     private JFileChooser fileChooser;
+
+    private JPanel colorPanel;
 
 
     private BufferedImage basicImage = new ProcessedImage().createABackground();
@@ -35,20 +36,24 @@ public class GUI extends JFrame implements ActionListener {
         setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
         openButton = new JButton("Open");
-        openButton.addActionListener(this);
+        openButton.addActionListener(e -> useOpenButton());
         counterLabel = new JLabel("You have uploaded: " + images.size() + " photos");
 
         makingButton = new JButton("Make an collage");
-        makingButton.addActionListener(this);
+        makingButton.addActionListener(e -> useMakingButton());
 
         saveButton = new JButton("Save");
-        saveButton.addActionListener(this);
+        saveButton.addActionListener(e -> useSaveButton());
+
+        colorButton = new JButton("Choose color of background");
+        colorButton.addActionListener(e -> useColorButton());
 
         JPanel buttonPanel = new JPanel();
         JPanel buttonPanel2 = new JPanel();
 
         buttonPanel.add(openButton);
         buttonPanel.add(counterLabel);
+        buttonPanel.add(colorButton);
 
 
         buttonPanel2.add(makingButton);
@@ -72,38 +77,49 @@ public class GUI extends JFrame implements ActionListener {
         SwingUtilities.invokeLater(GUI::new);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == openButton) {
-            int returnVal = fileChooser.showOpenDialog(this);
-            if (returnVal == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
-                try {
-                    images.add(ImageIO.read(file));
-                    counterLabel.setText("You have uploaded: " + images.size() + " photos");
-                } catch (Exception ex) {
-                    JOptionPane.showMessageDialog(this, "Error opening file.");
-                }
-            }
-        }else if (e.getSource() == makingButton){
-            int xShift = height / 8;
-            try{
-                for (int i = 0; i < images.size(); i++) {
-                    overlayImage(basicImage, images.get(i), xShift);
-                    xShift += (5 * height) / 8;
-                }
-            }catch (Exception ex){
-                JOptionPane.showMessageDialog(this, "Error creating collage.");
-            }
-
-        }else if (e.getSource() == saveButton){
+    public void useOpenButton() {
+        int returnVal = fileChooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fileChooser.getSelectedFile();
             try {
-                savingImages(basicImage, "UItest");
+                images.add(ImageIO.read(file));
+                counterLabel.setText("You have uploaded: " + images.size() + " photos");
             } catch (Exception ex) {
-                JOptionPane.showMessageDialog(this, "Error saving collage.");
+                JOptionPane.showMessageDialog(this, "Error opening file.");
             }
+        }
+    }
 
+    public void useMakingButton() {
+        int xShift = height / 8;
+        try {
+            for (int i = 0; i < images.size(); i++) {
+                overlayImage(basicImage, images.get(i), xShift);
+                xShift += (5 * height) / 8;
+            }
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error creating collage.");
+        }
+    }
+
+    public void useSaveButton() {
+        try {
+            savingImages(basicImage, "UItest");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error saving collage.");
         }
 
+    }
+
+    public void useColorButton(){
+        Color selectedColor = JColorChooser.showDialog(this, "Choose Color", Color.WHITE);
+        if (selectedColor != null) {
+            colorPanel.setBackground(selectedColor);
+            }
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
     }
 }
